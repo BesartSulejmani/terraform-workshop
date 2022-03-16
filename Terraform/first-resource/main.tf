31fb1d173cca425e110010b877ff12a495cd699b
besart.sulejmani <besart.sulejmani@linkit.nl>
2022-03-16 09:44:43 +0100

    added vnet to first-resource

--- Terraform/first-resource/main.tf
+++ Terraform/first-resource/main.tf
@@ -15,7 +15,15 @@ provider "azurerm" {
 }
 
 # Create resource group
-resource "azurerm_resource_group" "myRG" {
+resource "azurerm_resource_group" "myrg" {
   name = "tf-workshop-rg"
   location = "westeurope"
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "myvnet" {
+  name                = "tf-workshop-vnet01"
+  location            = azurerm_resource_group.myrg.location
+  resource_group_name = azurerm_resource_group.myrg.name
+  address_space       = ["10.0.0.0/16"]
 }
