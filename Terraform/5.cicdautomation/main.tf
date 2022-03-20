e78bb78904bf01e0231758ca7c0dd444d8cd5c9f
besart.sulejmani <besart.sulejmani@linkit.nl>
2022-03-20 21:47:59 +0100

    create cicdautomation folder and azure devops pipeline

---
+++ Terraform/5.cicdautomation/main.tf
@@ -0,0 +1,31 @@
+# Recommended using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  backend "azurerm" {
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.97.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create resource group
+resource "azurerm_resource_group" "myrg" {
+  name     = "tf-workshop-rg"
+  location = "westeurope"
+}
+
+# Create app plan and app service of kind linux
+module "myappservice" {
+  source = "./myapp"
+
+  rglocation = azurerm_resource_group.myrg.location
+  rgname = azurerm_resource_group.myrg.name
+}
